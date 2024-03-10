694435c82443f406951a26741d8b417ea8ccdb49
Dan Balasescu <smoogipoo@smgi.me>
2024-03-10 21:13:57 +0900

    Add catch ruleset support

---
+++ Sources/Shared/Mocks/osu.Game.Rulesets.Catch/UI/CatchPlayfield.cs
@@ -0,0 +1,14 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Rulesets.UI;
+
+// ReSharper disable once CheckNamespace
+
+namespace osu.Game.Rulesets.Catch.UI
+{
+    public class CatchPlayfield : Playfield
+    {
+        public const float WIDTH = 512;
+    }
+}
--- Sources/Shared/Mocks/osu.Game/Rulesets/Scoring/ScoreProcessor.cs
+++ Sources/Shared/Mocks/osu.Game/Rulesets/Scoring/ScoreProcessor.cs
@@ -1,6 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Collections.Generic;
 using osu.Framework.Bindables;
 using osu.Framework.Graphics.Containers;
 
@@ -10,7 +11,12 @@ namespace osu.Game.Rulesets.Scoring
 {
     public class ScoreProcessor : CompositeDrawable
     {
+        public const double MAX_SCORE = 1000000;
+
+        public Ruleset Ruleset = default!;
+        public BindableLong TotalScore = default!;
         public Bindable<double> Accuracy = default!;
         public Bindable<double> MaximumAccuracy = default!;
+        public Dictionary<HitResult, int> MaximumStatistics = default!;
     }
 }
