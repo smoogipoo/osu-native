d88d67a887b2cfbed1b57b0e65c532af0ba2db62
Dan Balasescu <smoogipoo@smgi.me>
2024-03-10 20:21:32 +0900

    Fix null beatmap ruleset during parsing

--- Sources/Shared/Mocks/osu.Game/Rulesets/RulesetStore.cs
+++ Sources/Shared/Mocks/osu.Game/Rulesets/RulesetStore.cs
@@ -1,7 +1,7 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
+using osu.Game.Native;
 
 // ReSharper disable once CheckNamespace
 
@@ -9,6 +9,6 @@ namespace osu.Game.Rulesets
 {
     public class RulesetStore
     {
-        public RulesetInfo? GetRuleset(int id) => throw new NotImplementedException();
+        public RulesetInfo GetRuleset(int id) => RulesetHelper.CreateRuleset(id).RulesetInfo;
     }
 }
