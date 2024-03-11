9fc03453a0cbc84a63636ca64a1f7ac8c9377243
Dan Balasescu <smoogipoo@smgi.me>
2024-03-11 14:22:14 +0900

    Add ruleset dropdown

--- Sources/osu.Game.Native.WebAssembly/wwwroot/main.js
+++ Sources/osu.Game.Native.WebAssembly/wwwroot/main.js
@@ -11,6 +11,9 @@ const {getAssemblyExports, getConfig} = await dotnet
 const config = getConfig();
 const exports = await getAssemblyExports(config.mainAssemblyName);
 
+let beatmap = null;
+let rulesetId = 0;
+
 function readFileAsText(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -20,15 +23,25 @@ function readFileAsText(file) {
     });
 }
 
-document.getElementById('beatmap-file').addEventListener('change', async (event) => {
-    const file = event.target.files[0];
-    const data = await readFileAsText(file);
+async function updateStarRating() {
+    if (beatmap === null)
+        return;
 
     const startTime = new Date();
-    const result = await exports.Program.ComputeDifficulty(data, 0, 0);
+    const result = await exports.Program.ComputeDifficulty(beatmap, Number(rulesetId), 0);
     const endTime = new Date();
 
     document.getElementById('result').innerHTML = Math.round(result * 100) / 100;
 
     console.log(`took ${endTime - startTime}ms`)
+}
+
+document.getElementById('beatmap-file').addEventListener('change', async (event) => {
+    beatmap = await readFileAsText(event.target.files[0]);
+    await updateStarRating();
+});
+
+document.getElementById('ruleset-dropdown').addEventListener('change', async (event) => {
+    rulesetId = event.target.value;
+    await updateStarRating();
 });
