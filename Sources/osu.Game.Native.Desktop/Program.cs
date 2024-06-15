cc0236513c74be2dd13f418e579961f97fb2e70d
Dan Balasescu <smoogipoo@smgi.me>
2024-06-15 19:50:18 +0900

    Add exception handling

--- Sources/osu.Game.Native.Desktop/Program.cs
+++ Sources/osu.Game.Native.Desktop/Program.cs
@@ -14,7 +14,7 @@ using osu.Game.Rulesets.Mods;
 
 // ReSharper disable once CheckNamespace
 
-public class Program
+public unsafe class Program
 {
     static Program()
     {
@@ -28,15 +28,52 @@ public class Program
         _ = Activator.CreateInstance<LegacyDecoder<Beatmap>.LegacySampleControlPoint>();
     }
 
-    [UnmanagedCallersOnly(EntryPoint = "ComputeDifficulty")]
-    public static unsafe double ComputeDifficulty(char* beatmapText, int rulesetId, uint mods)
-    {
-        WorkingBeatmap workingBeatmap = new StringBackedWorkingBeatmap(Marshal.PtrToStringAuto((IntPtr)beatmapText) ?? throw new InvalidOperationException("Empty beatmap."));
-        Ruleset ruleset = RulesetHelper.CreateRuleset(rulesetId);
-        Mod[] rulesetMods = ruleset.ConvertFromLegacyMods((LegacyMods)mods).ToArray();
+    private static delegate*<IntPtr, void> logger;
 
-        return ruleset.CreateDifficultyCalculator(workingBeatmap)
-                      .Calculate(rulesetMods)
-                      .StarRating;
+    [UnmanagedCallersOnly(EntryPoint = "SetLogger")]
+    public static void SetLogger(delegate*<IntPtr, void> handler)
+        => logger = handler;
+
+    [UnmanagedCallersOnly(EntryPoint = "ComputeDifficulty")]
+    public static ErrorCode ComputeDifficulty(char* beatmapTextPtr, int rulesetId, uint mods, double* starRating)
+    {
+        *starRating = 0;
+
+        try
+        {
+            string? beatmapText = Marshal.PtrToStringAuto((IntPtr)beatmapTextPtr);
+
+            if (string.IsNullOrEmpty(beatmapText))
+                return error(ErrorCode.EmptyBeatmap, "The provided beatmap was empty.");
+
+            WorkingBeatmap workingBeatmap = new StringBackedWorkingBeatmap(beatmapText);
+            Ruleset ruleset = RulesetHelper.CreateRuleset(rulesetId);
+            Mod[] rulesetMods = ruleset.ConvertFromLegacyMods((LegacyMods)mods).ToArray();
+
+            *starRating = ruleset.CreateDifficultyCalculator(workingBeatmap)
+                                 .Calculate(rulesetMods)
+                                 .StarRating;
+
+            return ErrorCode.Success;
+        }
+        catch (Exception ex)
+        {
+            return error(ErrorCode.Failure, ex.ToString());
+        }
+    }
+
+    private static ErrorCode error(ErrorCode code, string description)
+    {
+        if (logger != null)
+            logger(Marshal.StringToHGlobalAuto(description));
+
+        return code;
+    }
+
+    public enum ErrorCode : byte
+    {
+        Success = 0,
+        EmptyBeatmap = 1,
+        Failure = 255
     }
 }
