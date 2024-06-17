72b8c35be30eb6ebfdc750ab289fb4a98f0be0e3
Dan Balasescu <smoogipoo@smgi.me>
2024-06-18 05:29:28 +0900

    Update submodule + remove now-unnecessary AOT workarounds
    The types are now correctly annotated in the osu submodule.

--- Sources/osu.Game.Native.Desktop/Program.cs
+++ Sources/osu.Game.Native.Desktop/Program.cs
@@ -7,69 +7,15 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using osu.Game.Beatmaps;
-using osu.Game.Beatmaps.ControlPoints;
-using osu.Game.Beatmaps.Formats;
 using osu.Game.Beatmaps.Legacy;
 using osu.Game.Native;
 using osu.Game.Rulesets;
-using osu.Game.Rulesets.Catch.Mods;
-using osu.Game.Rulesets.Mania.Mods;
 using osu.Game.Rulesets.Mods;
-using osu.Game.Rulesets.Osu.Mods;
-using osu.Game.Rulesets.Taiko.Mods;
 
 // ReSharper disable once CheckNamespace
 
 public unsafe class Program
 {
-    static Program()
-    {
-        // This is kinda unfortunate, but I want to avoid mocking these.
-        // Sigh... Probably aaaaaaaaaalllllll of these should not use Activator.CreateInstance().
-
-        _ = Activator.CreateInstance<LegacyControlPointInfo>();
-        _ = Activator.CreateInstance<TimingControlPoint>();
-        _ = Activator.CreateInstance<SampleControlPoint>();
-        _ = Activator.CreateInstance<EffectControlPoint>();
-        _ = Activator.CreateInstance<DifficultyControlPoint>();
-        _ = Activator.CreateInstance<TimingControlPoint>();
-        _ = Activator.CreateInstance<LegacyDecoder<Beatmap>.LegacySampleControlPoint>();
-
-        _ = Activator.CreateInstance<OsuModTouchDevice>();
-        _ = Activator.CreateInstance<OsuModDoubleTime>();
-        _ = Activator.CreateInstance<OsuModHalfTime>();
-        _ = Activator.CreateInstance<OsuModEasy>();
-        _ = Activator.CreateInstance<OsuModHardRock>();
-        _ = Activator.CreateInstance<OsuModFlashlight>();
-        _ = Activator.CreateInstance<OsuModHidden>();
-        _ = Activator.CreateInstance<OsuModRelax>();
-
-        _ = Activator.CreateInstance<TaikoModDoubleTime>();
-        _ = Activator.CreateInstance<TaikoModHalfTime>();
-        _ = Activator.CreateInstance<TaikoModEasy>();
-        _ = Activator.CreateInstance<TaikoModHardRock>();
-
-        _ = Activator.CreateInstance<CatchModDoubleTime>();
-        _ = Activator.CreateInstance<CatchModHalfTime>();
-        _ = Activator.CreateInstance<CatchModEasy>();
-        _ = Activator.CreateInstance<CatchModHardRock>();
-
-        _ = Activator.CreateInstance<ManiaModDoubleTime>();
-        _ = Activator.CreateInstance<ManiaModHalfTime>();
-        _ = Activator.CreateInstance<ManiaModEasy>();
-        _ = Activator.CreateInstance<ManiaModHardRock>();
-        _ = Activator.CreateInstance<ManiaModKey1>();
-        _ = Activator.CreateInstance<ManiaModKey2>();
-        _ = Activator.CreateInstance<ManiaModKey3>();
-        _ = Activator.CreateInstance<ManiaModKey4>();
-        _ = Activator.CreateInstance<ManiaModKey5>();
-        _ = Activator.CreateInstance<ManiaModKey6>();
-        _ = Activator.CreateInstance<ManiaModKey7>();
-        _ = Activator.CreateInstance<ManiaModKey8>();
-        _ = Activator.CreateInstance<ManiaModKey9>();
-        _ = Activator.CreateInstance<ManiaModDualStages>();
-    }
-
     private static LogDelegate? logger;
 
     /// <summary>
