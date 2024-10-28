9355aca37b07397466d2f7076ddb14a58f91d630
Dan Balasescu <smoogipoo@smgi.me>
2024-10-28 12:42:14 +0900

    Bring up-to-date with latest osu!

---
+++ Sources/Shared/Stubs/osu.Game/Online/API/IAPIProvider.cs
@@ -0,0 +1,12 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+// ReSharper disable once CheckNamespace
+
+namespace osu.Game.Online.API
+{
+    public interface IAPIProvider
+    {
+        string WebsiteRootUrl { get; }
+    }
+}
